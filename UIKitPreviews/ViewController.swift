c23e6eec35803e901eeef3b2ac02c76c5a9d86cb
Adrian Navarro (Klutch) <KlutchTheDev@gmail.com>
2019-11-13 09:39:41 -0800

    Initial Commit

---
+++ UIKitPreviews/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UIKitPreviews
+//
+//  Created by Adrian Navarro on 11/13/19.
+//  Copyright © 2019 Adrian Navarro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
